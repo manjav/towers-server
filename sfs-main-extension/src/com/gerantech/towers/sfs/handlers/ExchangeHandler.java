e5fde109cc3b59f97054b7d3edd45f0403c0bb13
DESKTOP-F2U0GHU\ManJav <mansurjavadi@gmail.com>
2017-07-31 17:06:33 +0430

    insert new cards rewards to database

--- sfs-main-extension/src/com/gerantech/towers/sfs/handlers/ExchangeHandler.java
+++ sfs-main-extension/src/com/gerantech/towers/sfs/handlers/ExchangeHandler.java
@@ -29,12 +29,30 @@ public class ExchangeHandler extends BaseClientRequestHandler
     	int type = params.getInt("type");
     	if(params.containsKey("hards"))
     		hardsConfimed = params.getInt("hards");
-    	
+
 		Exchanger exchanger = ((Game)sender.getSession().getProperty("core")).exchanger;
 		Player player = ((Game)sender.getSession().getProperty("core")).player;
 		ExchangeItem item = exchanger.items.get(type);
 		int now = (int)Instant.now().getEpochSecond();
-		
+
+		if(ExchangeType.getCategory(type) == ExchangeType.S_30_CHEST)
+			item.outcomes = exchanger.getChestOutcomes(type);
+
+		// define new outcomes for inert query
+		IntIntMap updateMap = new IntIntMap();
+		IntIntMap insertMap = new IntIntMap();
+		int[] outk = item.outcomes.keys();
+		int r = 0;
+		while ( r < outk.length )
+		{
+			if ( player.resources.exists(outk[r]) )
+				updateMap.set(outk[r], 0);
+			else
+				insertMap.set(outk[r], item.outcomes.get(outk[r]));
+			//trace(r, outk[r], 0 );
+			r ++;
+		}
+
 		Boolean succeed = exchanger.exchange(item, now, hardsConfimed);
 		params.putBool("succeed", succeed);
 		params.putInt("now", now);
@@ -51,12 +69,13 @@ public class ExchangeHandler extends BaseClientRequestHandler
 		int[] outs = item.outcomes.keys();
 		for(int o = 0; o<outs.length; o++)
 			trace("outcomes", outs[o], item.outcomes.get(outs[o]));*/
-		trace("type:", type, "expiredAt:", item.expiredAt, "now:", now, "hardsConfimed:", hardsConfimed, "succeed:", succeed, "numExchanges:", item.numExchanges, "outcome:", item.outcome);
+		trace("type:", type, " ,expiredAt:", item.expiredAt, " ,now:", now, " ,outcomes:", item.outcomes.keys().length, " ,hardsConfimed:", hardsConfimed, " ,succeed:", succeed, " ,numExchanges:", item.numExchanges, " ,outcome:", item.outcome);
 
 		if(ExchangeType.getCategory(type) == ExchangeType.S_30_CHEST)
 		{
     		SFSArray sfsRewards = new SFSArray();
-    		for (int i : item.outcomes.keys())
+			int[] outKeys = item.outcomes.keys();
+    		for (int i : outKeys)
     		{
 	    		SFSObject so = new SFSObject();
 	    		so.putInt("t", i);
@@ -67,16 +86,19 @@ public class ExchangeHandler extends BaseClientRequestHandler
 		}
 
 		int outcome = ExchangeType.getCategory(type) == ExchangeType.S_20_BUILDING ? item.outcomes.keys()[0] : 0;
-		// update database
-		//add reqs to resources
-		IntIntMap resources = new IntIntMap();
-		for(int o:item.outcomes.keys())
-			resources.set(o, 0);
-        for(int r:item.requirements.keys())
-        	resources.set(r, 0);
 
+		// add reqs keys to query
+		outk = item.requirements.keys();
+		r = 0;
+		while ( r < outk.length ) {
+			updateMap.set(r, 0);
+			r ++;
+		}
+
+		// update database
 		try {
-			UserManager.updateResources(getParentExtension(), player, resources);
+			trace(UserManager.updateResources(getParentExtension(), player, updateMap));
+			trace(UserManager.insertResources(getParentExtension(), player, insertMap));
 			trace(UserManager.updateExchange(getParentExtension(), type, player.id, item.expiredAt, item.numExchanges, outcome));
 		} catch (Exception e) {
 			trace(e.getMessage());
