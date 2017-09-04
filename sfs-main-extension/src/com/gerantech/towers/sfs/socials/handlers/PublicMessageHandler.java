8f8b12dc3e6b858828a444dc36d9264d99975808
ManJav <mansurjavadi@gmail.com>
2017-09-05 01:03:18 +0430

    added accept, cancel and remove friendly battle handler

--- sfs-main-extension/src/com/gerantech/towers/sfs/socials/handlers/PublicMessageHandler.java
+++ sfs-main-extension/src/com/gerantech/towers/sfs/socials/handlers/PublicMessageHandler.java
@@ -1,10 +1,7 @@
 package com.gerantech.towers.sfs.socials.handlers;
 
 import com.gerantech.towers.sfs.Commands;
-import com.gerantech.towers.sfs.TowerExtension;
 import com.gerantech.towers.sfs.handlers.BattleAutoJoinHandler;
-import com.gt.towers.Game;
-import com.gt.towers.Player;
 import com.gt.towers.constants.MessageTypes;
 import com.smartfoxserver.v2.entities.Room;
 import com.smartfoxserver.v2.entities.User;
@@ -22,20 +19,22 @@ public class PublicMessageHandler extends BaseClientRequestHandler
         if( params.getShort("m") == MessageTypes.M30_FRIENDLY_BATTLE )
         {
             Integer friendlyBattleId = params.getInt("bid");
+
             if (friendlyBattleId == -1)
             {
                 Room room = BattleAutoJoinHandler.makeNewRoom(getApi(), getParentExtension().getParentZone(), sender, false, 0);
+                getParentExtension().getParentRoom().setProperty(room.getName(), true);
                 room.setProperty("isFriendly", true);
-                BattleAutoJoinHandler.join(getApi(), sender, room, "");//params.getText("bsu")
+                BattleAutoJoinHandler.join(getApi(), sender, room, "");
                 params.putInt("bid", room.getId());
             }
             else if (friendlyBattleId > -1)
             {
                 Room room = getParentExtension().getParentZone().getRoomById(friendlyBattleId);
-                if( params.getShort("st") == 3 )
+                if ( params.getShort("st") < 2 )
+                    BattleAutoJoinHandler.join(getApi(), sender, room, params.containsKey("sp") ? params.getText("s") : "");
+                else
                     getApi().removeRoom(room);
-                else if( params.getShort("st") == 1 )
-                    BattleAutoJoinHandler.join(getApi(), sender, room, "");//params.getText("bsu")
             }
         }
 
