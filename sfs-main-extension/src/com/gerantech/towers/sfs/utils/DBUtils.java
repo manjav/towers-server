ff47b6f161125fd4d5ce94e0859614c1f571b69b
ManJav <mansurjavadi@gmail.com>
2018-01-09 11:25:37 +0330

    new card level changed to -1

--- sfs-main-extension/src/com/gerantech/towers/sfs/utils/DBUtils.java
+++ sfs-main-extension/src/com/gerantech/towers/sfs/utils/DBUtils.java
@@ -115,7 +115,7 @@ public class DBUtils
         keyLen = keys.length;
         for (int i = 0; i < keyLen; i++)
         {
-            query += "('" + player.id + "', '" + keys[i] + "', '" + player.resources.get(keys[i]) + "', '" + (ResourceType.isCard(keys[i])?1:0) + "')";
+            query += "('" + player.id + "', '" + keys[i] + "', '" + player.resources.get(keys[i]) + "', '" + (ResourceType.isCard(keys[i])?player.buildings.get(keys[i]).get_level():0) + "')";
             query += i < keyLen - 1 ? ", " : ";";
         }
 
