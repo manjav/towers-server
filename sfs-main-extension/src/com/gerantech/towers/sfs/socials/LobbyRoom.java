a07ff22a70f6f460940aa17bb9e7eda3ff07a557
Babak <ssalimib@gmail.com>
2018-02-01 15:05:48 +0330

    Added previous request check condition

--- sfs-main-extension/src/com/gerantech/towers/sfs/socials/LobbyRoom.java
+++ sfs-main-extension/src/com/gerantech/towers/sfs/socials/LobbyRoom.java
@@ -4,6 +4,7 @@ import com.gerantech.towers.sfs.Commands;
 import com.gerantech.towers.sfs.inbox.InboxUtils;
 import com.gerantech.towers.sfs.socials.handlers.*;
 import com.gerantech.towers.sfs.utils.BattleUtils;
+import com.gerantech.towers.sfs.utils.DBUtils;
 import com.gt.towers.Game;
 import com.gt.towers.Player;
 import com.gt.towers.constants.MessageTypes;
@@ -12,7 +13,9 @@ import com.smartfoxserver.v2.entities.Room;
 import com.smartfoxserver.v2.entities.User;
 import com.smartfoxserver.v2.entities.data.ISFSArray;
 import com.smartfoxserver.v2.entities.data.ISFSObject;
+import com.smartfoxserver.v2.entities.data.SFSArray;
 import com.smartfoxserver.v2.entities.data.SFSObject;
+import com.smartfoxserver.v2.exceptions.SFSException;
 import com.smartfoxserver.v2.exceptions.SFSJoinRoomException;
 import com.smartfoxserver.v2.extensions.SFSExtension;
 
@@ -25,7 +28,8 @@ import java.time.Instant;
 public class LobbyRoom extends SFSExtension
 {
     private Room lobby;
-    public void init() {
+    public void init()
+    {
         lobby = getParentRoom();
 
         addEventHandler(SFSEventType.USER_JOIN_ROOM, LobbyRoomServerEventsHandler.class);
@@ -74,13 +78,14 @@ public class LobbyRoom extends SFSExtension
                 messages.removeElementAt(messages.size() - 1);
             }
         }
-        else if( mode == MessageTypes.M30_FRIENDLY_BATTLE ) {
+        else if( mode == MessageTypes.M30_FRIENDLY_BATTLE )
+        {
 
-            // cancel requested battle by owner
+            // cancel previous requested battle by owner
             ISFSObject message = getMyRequestedBattle(params, game.player);
             if (message != null) {
-                params.putShort("st", (short) 3);
-                message.putShort("st", (short) 3);
+                params.putShort("st", (short) 3); // cancel this new battle request
+                message.putShort("st", (short) 3); // cancel previous battle request
                 Room room = getParentZone().getRoomById(message.getInt("bid"));
                 if (room != null)
                     getApi().leaveRoom(sender, room);
@@ -89,9 +94,11 @@ public class LobbyRoom extends SFSExtension
 
             // join to an available battle
             message = getAvailableBattle(params);
-            if (message != null) {
-                Room room = getParentZone().getRoomById(params.getInt("bid"));
-                if (room != null) {
+            if (message != null)
+            {
+                Room room = getParentZone().getRoomById(params.getInt("bid")); // battle room
+                if (room != null)
+                {
                     BattleUtils.getInstance().join(sender, room, "");
 
                     params.putText("o", game.player.nickName);
@@ -120,11 +127,31 @@ public class LobbyRoom extends SFSExtension
             if (params.getShort("st") > 0)
                 return;
 
+            // make new lobby battle
             BattleUtils battleUtils = BattleUtils.getInstance();
             Room room = battleUtils.make(sender, false, 0, 1, false);
             lobby.setProperty(room.getName(), true);
             battleUtils.join(sender, room, "");
             params.putInt("bid", room.getId());
+        }
+        else if ( mode == MessageTypes.M20_DONATE )
+        {
+            trace("\n\t..::DONATION::..\n");
+            params.putInt("did", 1000);
+            params.getDump();
+
+            //check card request or card donate. state=0 --> Request | state=1 --> Donate
+            if ( params.getShort("st") == 0 ) // Request Card
+            {
+                if ( checkPreviousCardRequest(params) )
+                    return;
+                messages.addSFSObject(params);
+            }
+            else if ( params.getShort("st") == 1 ) // Donate Card
+            {
+                getRequestedCardNum(params);
+            }
+            return;
         }
         else if (MessageTypes.isConfirm(mode))
         {
@@ -140,6 +167,43 @@ public class LobbyRoom extends SFSExtension
         messages.addSFSObject(params);
     }
 
+    private void getRequestedCardNum(ISFSObject params)
+    {
+        try {
+            DBUtils db = new DBUtils();
+            SFSArray resources = db.getResources(params.getInt("i"));
+            trace("player resources:");
+            resources.getDump();
+            /*for (int i = 0; i < resources.size(); i++) {
+                for (int j = 0; j < resources.getSFSArray(i).size(); j++) {
+                }
+            }*/
+        } catch (SFSException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private Boolean checkPreviousCardRequest(ISFSObject params) // returns true if previous card req exists
+    {
+        ISFSArray messages = messageQueue();
+        for (int i = messages.size()-1; i > 0; i--)
+        {
+            ISFSObject message = messages.getSFSObject(i);
+            Boolean isDonate = ( message.getShort("m") == MessageTypes.M20_DONATE );
+            if ( isDonate && message.getShort("st") == 0 && message.getInt("i").equals(params.getInt("i")) )
+            {
+                trace("ERROR-donation: There is a previous requests ...");
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private void updateCardNum(int playerId, int cardType, int value)
+    {
+        DBUtils db = DBUtils.getInstance();
+    }
+
     private int getRelatedConfirm(ISFSArray messages, ISFSObject params)
     {
         int msgSize = messages.size();
@@ -149,7 +213,7 @@ public class LobbyRoom extends SFSExtension
         return -1;
     }
 
-    private ISFSObject getMyRequestedBattle(ISFSObject params, Player player)
+    private ISFSObject  getMyRequestedBattle(ISFSObject params, Player player)
     {
         ISFSArray messages = messageQueue();
         int msgSize = messages.size();
@@ -168,6 +232,16 @@ public class LobbyRoom extends SFSExtension
         int msgSize = messages.size();
         for (int i = msgSize-1; i >=0; i--)
             if (messages.getSFSObject(i).getShort("m") == MessageTypes.M30_FRIENDLY_BATTLE && params.getShort("st") == 0 && messages.getSFSObject(i).getShort("st") == 0 && messages.getSFSObject(i).getInt("bid").equals(params.getInt("bid")))
+                return messages.getSFSObject(i);
+
+        return null;
+    }
+    private ISFSObject getAvailableDonation(ISFSObject params)
+    {
+        ISFSArray messages = messageQueue();
+        int msgSize = messages.size();
+        for (int i = msgSize-1; i >=0; i--)
+            if (messages.getSFSObject(i).getShort("m") == MessageTypes.M20_DONATE && params.getShort("st") == 0 && messages.getSFSObject(i).getShort("st") == 0 && messages.getSFSObject(i).getInt("did").equals(params.getInt("did")))
                 return messages.getSFSObject(i);
 
         return null;
