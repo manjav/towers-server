9fb095a3daea1cb7630a1efe8892385754f91500
ManJav <mansurjavadi@gmail.com>
2018-02-28 13:27:29 +0330

    added more select name ability

--- sfs-main-extension/src/com/gerantech/towers/sfs/handlers/SelectNameRequestHandler.java
+++ sfs-main-extension/src/com/gerantech/towers/sfs/handlers/SelectNameRequestHandler.java
@@ -2,7 +2,11 @@ package com.gerantech.towers.sfs.handlers;
 
 import java.sql.SQLException;
 
+import com.gerantech.towers.sfs.Commands;
+import com.gerantech.towers.sfs.TowerExtension;
 import com.gt.towers.Game;
+import com.gt.towers.constants.ResourceType;
+import com.gt.towers.exchanges.ExchangeItem;
 import com.smartfoxserver.v2.db.IDBManager;
 import com.smartfoxserver.v2.entities.User;
 import com.smartfoxserver.v2.entities.data.ISFSObject;
@@ -14,22 +18,37 @@ import com.smartfoxserver.v2.extensions.BaseClientRequestHandler;
  */
 public class SelectNameRequestHandler extends BaseClientRequestHandler 
 {
-	
+	static int RESPONSE_SUCCEED = 0;
+	static int RESPONSE_WRONG_SIZE = 1;
+	static int RESPONSE_NOT_ENOUG_REQUIREMENTS = 2;
+
 	public SelectNameRequestHandler() {}
 
 	public void handleClientRequest(User sender, ISFSObject params)
     {
-		params.putBool("succeed", true);
 		Game game = ((Game)sender.getSession().getProperty("core"));
 
 		String name = params.getUtfString("name");
-		trace(name, game.player.id);
-		if(name.length() < game.loginData.nameMinLen || name.length() > game.loginData.nameMaxLen)
+		trace(name, game.player.id, game.player.nickName);
+		if( name.length() < game.loginData.nameMinLen || name.length() > game.loginData.nameMaxLen )
 		{
 			params.putBool("succeed", false);
 			params.putText("errorCode", "popup_select_name_size");
-			send("selectName", params, sender);
+			params.putInt("response", RESPONSE_WRONG_SIZE);
+			send(Commands.SELECT_NAME, params, sender);
 			return;
+		}
+
+		if( game.player.nickName != "guest" )
+		{
+			ExchangeItem ei = new ExchangeItem(-1, ResourceType.CURRENCY_HARD, 100, -1, -1, 1, 0);
+			if( !(((TowerExtension) getParentExtension()).exchangeHandler).exchange(game, ei, 0, 0) )
+			{
+				params.putBool("succeed", false);
+				params.putInt("response", RESPONSE_NOT_ENOUG_REQUIREMENTS);
+				send(Commands.SELECT_NAME, params, sender);
+				return;
+			}
 		}
 
 		IDBManager dbManager = getParentExtension().getParentZone().getDBManager();
@@ -41,6 +60,8 @@ public class SelectNameRequestHandler extends BaseClientRequestHandler
 			trace(e.getMessage());
 		}
 		game.player.nickName = name;
-		send("selectName", params, sender);
+		params.putBool("succeed", true);
+		params.putInt("response", RESPONSE_SUCCEED);
+		send(Commands.SELECT_NAME, params, sender);
     }
 }
