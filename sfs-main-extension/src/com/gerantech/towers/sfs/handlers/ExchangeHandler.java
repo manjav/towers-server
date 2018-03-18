d2dc56dfddbd3ec9522c890c2b3e63cfd20a59e6
Babak <ssalimib@gmail.com>
2018-03-18 11:13:49 +0330

    Added a new method for donation exchange in exchange handler

--- sfs-main-extension/src/com/gerantech/towers/sfs/handlers/ExchangeHandler.java
+++ sfs-main-extension/src/com/gerantech/towers/sfs/handlers/ExchangeHandler.java
@@ -6,6 +6,7 @@ import com.gerantech.towers.sfs.callbacks.MapChangeCallback;
 import com.gerantech.towers.sfs.utils.DBUtils;
 import com.gt.towers.Game;
 import com.gt.towers.constants.ExchangeType;
+import com.gt.towers.exchanges.ExchangeDonateItem;
 import com.gt.towers.exchanges.ExchangeItem;
 import com.smartfoxserver.v2.entities.User;
 import com.smartfoxserver.v2.entities.data.ISFSObject;
@@ -84,6 +85,51 @@ public class ExchangeHandler extends BaseClientRequestHandler
 		Boolean succeed = false;
 		try {
 			succeed = game.exchanger.exchange(item, now, hardsConfimed);
+		} catch (Exception e) { e.printStackTrace(); }
+		game.player.resources.changeCallback = null;
+		if( !succeed )
+			return false;
+
+		// logs .....
+		/*int[] inserts = mapChangeCallback.inserts.keys();
+		for(int i = 0; i<inserts.length; i++)
+			trace("inserts", inserts[i], mapChangeCallback.inserts.get(inserts[i]));
+		int[] updates = mapChangeCallback.updates.keys();
+		for(int o = 0; o<updates.length; o++)
+			trace("updates", updates[o], mapChangeCallback.inserts.get(updates[o]));*/
+		trace("Exchange => type:", item.type, " ,expiredAt:", item.expiredAt, " ,now:", now, " ,outcomes:", item.outcomes==null?"":item.outcomes.keys().length, " ,hardsConfimed:", hardsConfimed, " ,succeed:", succeed, " ,numExchanges:", item.numExchanges, " ,outcome:", item.outcome);
+
+		// Run db queries
+		DBUtils dbUtils = DBUtils.getInstance();
+		try
+		{
+			dbUtils.updateResources(game.player, mapChangeCallback.updates);
+			dbUtils.insertResources(game.player, mapChangeCallback.inserts);
+			if( item.isChest() )
+				dbUtils.updateExchange(item.type, game.player.id, item.expiredAt, item.numExchanges, item.outcome);
+		}
+		catch (Exception e)
+		{
+			e.printStackTrace();
+			return false;
+		}
+		return true;
+	}
+	public boolean exchange(Game game, ExchangeDonateItem item, int now, int hardsConfimed)
+	{
+
+
+		/*String log = "";
+		int[] keys = game.player.resources.keys();
+		for(int i = 0; i<keys.length; i++)
+			log += (keys[i] + ": " +game.player.resources.get(keys[i]) +" , " );
+		trace ( log );*/
+
+		MapChangeCallback mapChangeCallback = new MapChangeCallback();
+		game.player.resources.changeCallback = mapChangeCallback;
+		Boolean succeed = false;
+		try {
+			succeed = game.exchanger.exchangeDonate(item, now, hardsConfimed);
 		} catch (Exception e) { e.printStackTrace(); }
 		game.player.resources.changeCallback = null;
 		if( !succeed )
