70f930393864be54bc2ab2e8dbaac581365abbfd
ManJav <mansurjavadi@gmail.com>
2018-03-29 19:22:36 +0430

    added player names into issues

--- sfs-main-extension/src/com/gerantech/towers/sfs/administration/IssueGetHandler.java
+++ sfs-main-extension/src/com/gerantech/towers/sfs/administration/IssueGetHandler.java
@@ -16,7 +16,7 @@ public class IssueGetHandler extends BaseClientRequestHandler
 	public void handleClientRequest(User sender, ISFSObject params)
     {
  		try {
-			params.putSFSArray("issues", getParentExtension().getParentZone().getDBManager().executeQuery("SELECT id, player_id, description, status, UNIX_TIMESTAMP(report_at) FROM bugs ORDER BY id DESC LIMIT 100;", new Object[] {}));
+			params.putSFSArray("issues", getParentExtension().getParentZone().getDBManager().executeQuery("SELECT bugs.id, bugs.player_id, bugs.description, bugs.status, UNIX_TIMESTAMP(bugs.report_at), players.name as sender FROM bugs INNER JOIN players ON bugs.player_id = players.id ORDER BY bugs.id DESC LIMIT 100;", new Object[] {}));
 		} catch (SQLException e) { e.printStackTrace(); }
 		send(Commands.ISSUE_GET, params, sender);
     }
