d2f9eb827ea343b9a7cbe62dae652395630625c3
ManJav <mansurjavadi@gmail.com>
2018-04-08 15:16:16 +0430

    get all issues of unique player

--- sfs-main-extension/src/com/gerantech/towers/sfs/administration/IssueGetHandler.java
+++ sfs-main-extension/src/com/gerantech/towers/sfs/administration/IssueGetHandler.java
@@ -15,8 +15,13 @@ public class IssueGetHandler extends BaseClientRequestHandler
 {
 	public void handleClientRequest(User sender, ISFSObject params)
     {
+    	String query = "SELECT bugs.id, bugs.player_id, bugs.description, bugs.status, UNIX_TIMESTAMP(bugs.report_at) as date, players.name as sender FROM bugs INNER JOIN players ON bugs.player_id = players.id";
+    	if( params.containsKey("id") )
+			query += " WHERE bugs.player_id=" + params.getInt("id");
+		query += " ORDER BY bugs.id DESC LIMIT 100;";
+
  		try {
-			params.putSFSArray("issues", getParentExtension().getParentZone().getDBManager().executeQuery("SELECT bugs.id, bugs.player_id, bugs.description, bugs.status, UNIX_TIMESTAMP(bugs.report_at) as date, players.name as sender FROM bugs INNER JOIN players ON bugs.player_id = players.id ORDER BY bugs.id DESC LIMIT 100;", new Object[] {}));
+			params.putSFSArray("issues", getParentExtension().getParentZone().getDBManager().executeQuery(query, new Object[] {}));
 		} catch (SQLException e) { e.printStackTrace(); }
 		send(Commands.ISSUE_GET, params, sender);
     }
