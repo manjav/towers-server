0ad8475b3c708c60bc37b404dee004f4b28e9a43
ManJav <mansurjavadi@gmail.com>
2018-04-10 10:35:04 +0430

    reset weekly battles operation, moved to joining

--- sfs-main-extension/src/com/gerantech/towers/sfs/socials/handlers/LobbyJoinHandler.java
+++ sfs-main-extension/src/com/gerantech/towers/sfs/socials/handlers/LobbyJoinHandler.java
@@ -2,8 +2,14 @@ package com.gerantech.towers.sfs.socials.handlers;
 
 import com.gerantech.towers.sfs.Commands;
 import com.gerantech.towers.sfs.socials.LobbyUtils;
+import com.gt.hazel.RankData;
 import com.gt.towers.Game;
+import com.gt.towers.Player;
 import com.gt.towers.constants.MessageTypes;
+import com.gt.towers.constants.ResourceType;
+import com.hazelcast.config.Config;
+import com.hazelcast.core.Hazelcast;
+import com.hazelcast.core.IMap;
 import com.smartfoxserver.v2.entities.Room;
 import com.smartfoxserver.v2.entities.User;
 import com.smartfoxserver.v2.entities.data.ISFSArray;
@@ -12,6 +18,7 @@ import com.smartfoxserver.v2.entities.data.SFSObject;
 import com.smartfoxserver.v2.exceptions.SFSJoinRoomException;
 import com.smartfoxserver.v2.extensions.BaseClientRequestHandler;
 
+import java.sql.SQLException;
 import java.time.Instant;
 
 /**
@@ -39,6 +46,20 @@ public class LobbyJoinHandler extends BaseClientRequestHandler
             } catch (SFSJoinRoomException e) {
                 e.printStackTrace();
             }
+
+            // reset weekly battles
+            try {
+                getParentExtension().getParentZone().getDBManager().executeUpdate("UPDATE resources SET count= 0 WHERE type=1204 AND count != 0 AND player_id = " + game.player.id, new Object[]{});
+            } catch (SQLException e) { e.printStackTrace(); }
+
+            IMap<Integer, RankData> users = Hazelcast.getOrCreateHazelcastInstance(new Config("aaa")).getMap("users");
+            game.player.resources.set(ResourceType.BATTLES_COUNT_WEEKLY, 0);
+            RankData rd = new RankData(game.player.id, game.player.nickName,  game.player.get_point(), 0);
+            if( users.containsKey(game.player.id) )
+                users.replace(game.player.id, rd);
+            else
+                users.put(game.player.id, rd);
+
             params.putInt("response", MessageTypes.RESPONSE_SUCCEED);
         }
         else if( privacy == 1 )
--- sfs-main-extension/src/com/gerantech/towers/sfs/socials/handlers/LobbyLeaveHandler.java
+++ sfs-main-extension/src/com/gerantech/towers/sfs/socials/handlers/LobbyLeaveHandler.java
@@ -1,18 +1,9 @@
 package com.gerantech.towers.sfs.socials.handlers;
 
-import com.gt.hazel.RankData;
-import com.gt.towers.Game;
-import com.gt.towers.Player;
-import com.gt.towers.constants.ResourceType;
-import com.hazelcast.config.Config;
-import com.hazelcast.core.Hazelcast;
-import com.hazelcast.core.IMap;
 import com.smartfoxserver.v2.entities.Room;
 import com.smartfoxserver.v2.entities.User;
 import com.smartfoxserver.v2.entities.data.ISFSObject;
 import com.smartfoxserver.v2.extensions.BaseClientRequestHandler;
-
-import java.sql.SQLException;
 
 /**
  * Created by ManJav on 8/24/2017.
@@ -29,19 +20,5 @@ public class LobbyLeaveHandler extends BaseClientRequestHandler
         for ( Room r:sender.getJoinedRooms() )
             if( r.getGroupId().equals("lobbies") )
                 getApi().leaveRoom(sender, r);
-
-        // reset weekly battles
-        Player player = ((Game)sender.getSession().getProperty("core")).player;
-        try {
-            getParentExtension().getParentZone().getDBManager().executeUpdate("UPDATE resources SET count= 0 WHERE type=1204 AND count != 0 AND player_id = " + player.id, new Object[]{});
-        } catch (SQLException e) { e.printStackTrace(); }
-
-        IMap<Integer, RankData> users = Hazelcast.getOrCreateHazelcastInstance(new Config("aaa")).getMap("users");
-        player.resources.set(ResourceType.BATTLES_COUNT_WEEKLY, 0);
-        RankData rd = new RankData(player.id, player.nickName,  player.get_point(), 0);
-        if( users.containsKey(player.id))
-            users.replace(player.id, rd);
-        else
-            users.put(player.id, rd);
     }
 }
