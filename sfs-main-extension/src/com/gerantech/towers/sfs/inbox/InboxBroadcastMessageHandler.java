a35a9e10ac06fe80baf7ddcaa5740bd3bbe7d5d3
ManJav <mansurjavadi@gmail.com>
2018-04-10 10:33:50 +0430

    towerstory renamed to KOOT

--- sfs-main-extension/src/com/gerantech/towers/sfs/inbox/InboxBroadcastMessageHandler.java
+++ sfs-main-extension/src/com/gerantech/towers/sfs/inbox/InboxBroadcastMessageHandler.java
@@ -20,7 +20,7 @@ public class InboxBroadcastMessageHandler extends BaseClientRequestHandler
 
         int delivered = 0;
         for (int i = 0; i < receiverIds.length; i++)
-            delivered += InboxUtils.getInstance().send(params.getShort("type"),params.getUtfString("text"), "TowerStory", 10000, receiverIds[i], params.getText("data"));
+            delivered += InboxUtils.getInstance().send(params.getShort("type"),params.getUtfString("text"), "KOOT", 10000, receiverIds[i], params.getText("data"));
 
         if ( params.getBool("isPush") )
             OneSignalUtils.getInstance().send(params.getUtfString("text"), params.getText("data"), receiverIds);
