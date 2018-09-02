6d03ca04fd68579ed26931df876e19d07427920d
ManJav <mansurjavadi@gmail.com>
2018-09-03 02:04:03 +0430

    get profile faster

--- sfs-main-extension/src/com/gerantech/towers/sfs/handlers/ProfileRequestHandler.java
+++ sfs-main-extension/src/com/gerantech/towers/sfs/handlers/ProfileRequestHandler.java
@@ -1,9 +1,9 @@
 package com.gerantech.towers.sfs.handlers;
 
+import com.gerantech.towers.sfs.Commands;
 import com.gerantech.towers.sfs.socials.LobbyUtils;
 import com.gerantech.towers.sfs.utils.PasswordGenerator;
 import com.gt.data.LobbyData;
-import com.smartfoxserver.v2.api.CreateRoomSettings;
 import com.smartfoxserver.v2.db.IDBManager;
 import com.smartfoxserver.v2.entities.User;
 import com.smartfoxserver.v2.entities.data.ISFSArray;
@@ -18,25 +18,16 @@ import java.sql.SQLException;
  */
 public class ProfileRequestHandler extends BaseClientRequestHandler
 {
-	public ProfileRequestHandler() {}
 	public void handleClientRequest(User sender, ISFSObject params)
     {
 		IDBManager dbManager = getParentExtension().getParentZone().getDBManager();
 		int playerId = params.getInt("id");
 
 		//  -=-=-=-=-=-=-=-=-  add resources data  -=-=-=-=-=-=-=-=-
-		String query = "SELECT type, count FROM resources WHERE player_id=" + playerId + " AND (type=1000 OR type=1001 OR type=1201 OR type=1202" ;
-		if( params.containsKey("am") )
-			query += " OR type=1002 OR type=1003 OR type=1004 OR type=1203 OR type=1204 OR type=1211)" ;
-		else
-			query += ")" ;
-
-		ISFSArray featuresArray = null;
+		String query = "SELECT type, count, level FROM resources WHERE player_id = " + playerId + (params.containsKey("am") ? ";" : " AND (type=1000 OR type=1001 OR type=1201 OR type=1202);") ;
 		try {
-			featuresArray = dbManager.executeQuery(query, new Object[]{});
-		} catch (SQLException e) { trace(e.getMessage()); }
-		params.putSFSArray("features", featuresArray );
-
+			params.putSFSArray("resources", dbManager.executeQuery(query, new Object[]{}));
+		} catch (SQLException e) { e.printStackTrace(); }
 
 		//  -=-=-=-=-=-=-=-=-  add player data  -=-=-=-=-=-=-=-=-
 		if( params.containsKey("pd") )
@@ -45,29 +36,9 @@ public class ProfileRequestHandler extends BaseClientRequestHandler
 			ISFSArray dataArray = null;
 			try {
 				dataArray = dbManager.executeQuery(query, new Object[]{});
-			} catch (SQLException e) { trace(e.getMessage()); }
+			} catch (SQLException e) { e.printStackTrace(); }
 			params.putSFSObject("pd", dataArray.getSFSObject(0));
 		}
-
-		/*//  -=-=-=-=-=-=-=-=-  add quests data  -=-=-=-=-=-=-=-=-
-		ISFSArray questsArray = null;
-		try {
-			questsArray = dbManager.executeQuery("SELECT `index` FROM quests WHERE player_id=" + playerId + " AND score>0 ORDER BY `index` DESC LIMIT 0, 1", new Object[]{});
-		} catch (SQLException e) { trace(e.getMessage()); }
-		if( questsArray.size() > 0 )
-		{
-			SFSObject q = new SFSObject();
-			q.putInt("count", questsArray.getSFSObject(0).getInt("index") + 1);
-			q.putInt("type", 5000);
-			featuresArray.addSFSObject(q);
-		}*/
-
-		//  -=-=-=-=-=-=-=-=-  add buildings data  -=-=-=-=-=-=-=-=-
-		ISFSArray buildingArray = null;
-		try {
-			buildingArray = dbManager.executeQuery("SELECT type, level FROM resources WHERE player_id=" + playerId + " AND (type<1000 AND type>0) LIMIT 100", new Object[]{});
-		} catch (SQLException e) { trace(e.getMessage()); }
-		params.putSFSArray("buildings", buildingArray );
 
 		//  -=-=-=-=-=-=-=-=-  add lobby data  -=-=-=-=-=-=-=-=-
 		LobbyData lobbyData = null;
@@ -83,6 +54,6 @@ public class ProfileRequestHandler extends BaseClientRequestHandler
 		params.putText("tag", PasswordGenerator.getInvitationCode(playerId));
 
 		//trace(params.getDump());
-		send("profile", params, sender);
+		send(Commands.PROFILE, params, sender);
     }
 }
