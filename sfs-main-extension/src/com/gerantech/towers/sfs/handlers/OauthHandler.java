c379636e1ade1ccfe5b0f008a2090ac275b63cd2
ManJav <mansurjavadi@gmail.com>
2018-09-03 01:59:16 +0430

    fixed switch oauth accounts bug

--- sfs-main-extension/src/com/gerantech/towers/sfs/handlers/OauthHandler.java
+++ sfs-main-extension/src/com/gerantech/towers/sfs/handlers/OauthHandler.java
@@ -1,5 +1,6 @@
 package com.gerantech.towers.sfs.handlers;
 
+import com.gerantech.towers.sfs.Commands;
 import com.gt.towers.Game;
 import com.smartfoxserver.v2.db.IDBManager;
 import com.smartfoxserver.v2.entities.User;
@@ -7,62 +8,50 @@ import com.smartfoxserver.v2.entities.data.ISFSArray;
 import com.smartfoxserver.v2.entities.data.ISFSObject;
 import com.smartfoxserver.v2.extensions.BaseClientRequestHandler;
 
+import java.sql.SQLException;
+
 /**
  * @author ManJav
  *
  */
 public class OauthHandler extends BaseClientRequestHandler 
 {
-	public OauthHandler() {}
 	public void handleClientRequest(User sender, ISFSObject params)
     {
 		int playerId = ((Game)sender.getSession().getProperty("core")).player.id;
-		String playerName = null;
-		String playerPassword = null;
-		
-		boolean needInsert = true;
 		IDBManager dbManager = getParentExtension().getParentZone().getDBManager();
 	
 		try {
-			
-			// retrieve user that saved account before
-	        ISFSArray res = dbManager.executeQuery("SELECT type, player_id FROM accounts WHERE id='"+params.getText("accountId")+"' OR player_id="+playerId, new Object[] {});
-	        if(res.size() > 0)
-	        {
-	        	playerId = res.getSFSObject(0).getInt("player_id");
-	        	
-	        	needInsert = !linkExists(res, params.getInt("accountType"));
-		        ISFSArray res2 = dbManager.executeQuery("SELECT name, password FROM players WHERE id="+playerId, new Object[] {});
-		        if(res2.size() > 0)
-		        {
-		        	playerName = res2.getSFSObject(0).getText("name");
-		        	playerPassword = res2.getSFSObject(0).getText("password");
-		        }
-	        }
-	        if( needInsert )
-	        {
-	        	String q = "INSERT INTO accounts (`player_id`, `type`, `id`, `name`, `image_url`) VALUES ('" + playerId + "', '" + params.getInt("accountType") + "', '" + params.getText("accountId") + "', '" + params.getText("accountName") + "', '" + params.getText("accountImageURL") + "');";
-	        	dbManager.executeInsert(q, new Object[] {});
-	        }
-		} catch (Exception e) {
-			trace(e.getMessage());
-		}
-		
-		// sent params to sender
-		params.putInt("playerId", playerId);
-		if( playerName != null )
-			params.putText("playerName", playerName);
-		if( playerPassword != null )
-			params.putText("playerPassword", playerPassword);
-		send("oauth", params, sender);
-		//trace(params.getDump());
 
+			// retrieve user that saved account before
+			ISFSArray accounts = dbManager.executeQuery("SELECT type, player_id FROM accounts WHERE id='" + params.getText("accountId") + "'", new Object[] {});
+			boolean needInsert = accounts.size() == 0;
+			if( !needInsert && playerId != accounts.getSFSObject(0).getInt("player_id") )// if exists player and his id equals player id
+	        {
+				playerId = accounts.getSFSObject(0).getInt("player_id");
+				ISFSArray players = dbManager.executeQuery("SELECT name, password FROM players WHERE id=" + playerId, new Object[] {});
+				if( players.size() > 0 )
+				{
+					params.putText("playerName",		players.getSFSObject(0).getText("name"));
+					params.putText("playerPassword",	players.getSFSObject(0).getText("password"));
+				}
+				needInsert = !linkExists(accounts, params.getInt("accountType")); // already stored
+			}
+
+			if( needInsert )
+				dbManager.executeInsert("INSERT INTO accounts (`player_id`, `type`, `id`, `name`, `image_url`) VALUES ('" + playerId + "', '" + params.getInt("accountType") + "', '" + params.getText("accountId") + "', '" + params.getText("accountName") + "', '" + params.getText("accountImageURL") + "');", new Object[] {});
+
+		} catch (SQLException e) { e.printStackTrace(); }
+
+		params.putInt("playerId", playerId);
+		send(Commands.OAUTH, params, sender);
     }
-	private boolean linkExists(ISFSArray res, int accountType )
+
+	private boolean linkExists(ISFSArray accounts, int accountType )
 	{
 		boolean ret = false;
-		for(int i=0; i<res.size(); i++)
-    		if( res.getSFSObject(i).getInt("type") == accountType )
+		for(int i=0; i<accounts.size(); i++)
+    		if( accounts.getSFSObject(i).getInt("type") == accountType )
 				ret = true;
 		return ret;
 	}
