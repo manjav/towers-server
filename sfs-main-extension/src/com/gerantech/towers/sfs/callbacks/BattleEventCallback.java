9c820f3567b988e1ba47857767df060561fd2f88
ManJav\Yoga <mansurjavadi@gmail.com>
2019-01-04 11:57:45 +0330

    new round of battles tweaked

--- sfs-main-extension/src/com/gerantech/towers/sfs/callbacks/BattleEventCallback.java
+++ sfs-main-extension/src/com/gerantech/towers/sfs/callbacks/BattleEventCallback.java
@@ -1,11 +1,14 @@
 package com.gerantech.towers.sfs.callbacks;
 
+import com.gerantech.towers.sfs.Commands;
 import com.gerantech.towers.sfs.battle.BattleRoom;
+import com.gt.towers.battle.BattleField;
 import com.gt.towers.battle.GameObject;
 import com.gt.towers.battle.fieldes.FieldData;
 import com.gt.towers.battle.units.Unit;
 import com.gt.towers.events.BattleEvent;
 import com.gt.towers.events.EventCallback;
+import com.smartfoxserver.v2.entities.data.SFSObject;
 import haxe.root.Array;
 
 import java.util.Iterator;
@@ -29,15 +32,26 @@ public class BattleEventCallback implements EventCallback
     @Override
     public void dispatch(int id, String type, Object data)
     {
+        // only headquarter battle is allive
+        if( battleRoom.battleField.state >= BattleField.STATE_4_ENDED || !battleRoom.battleField.field.type.equals(FieldData.TYPE_HEADQUARTER) )
+            return;
+
+        // when units disposed
         if( type == BattleEvent.STATE_CHANGE && (int) data == GameObject.STATE_8_DIPOSED )
         {
-            if( battleRoom.battleField.field.type.equals(FieldData.TYPE_HEADQUARTER) )
+            if( battleRoom.battleField.units.get(id).card.type == 201 || battleRoom.battleField.units.get(id).card.type == 221 )
             {
                 int other = battleRoom.battleField.units.get(id).side == 0 ? 1 : 0;
                 if( battleRoom.battleField.units.get(id).card.type == 201 )
                     battleRoom.endCalculator.scores[other] = Math.min(3, battleRoom.endCalculator.scores[other] + 3);
                 if( battleRoom.battleField.units.get(id).card.type == 221 )
                     battleRoom.endCalculator.scores[other] ++;
+
+                SFSObject params = new SFSObject();
+                params.putInt("winner", other);
+                params.putInt("0", battleRoom.endCalculator.scores[0]);
+                params.putInt("1", battleRoom.endCalculator.scores[1]);
+                battleRoom.send(Commands.BATTLE_NEW_ROUND, params, battleRoom.getParentRoom().getUserList());
             }
         }
     }
