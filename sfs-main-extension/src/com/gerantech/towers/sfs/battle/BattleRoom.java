c156209b7494436ecaa3d81ab529b1f3273bd43b
Mansour <mansurjavadi@gmail.com>
2019-03-14 09:27:28 +0330

    query where condition changed to id for performance improvement

--- sfs-main-extension/src/com/gerantech/towers/sfs/battle/BattleRoom.java
+++ sfs-main-extension/src/com/gerantech/towers/sfs/battle/BattleRoom.java
@@ -486,7 +486,7 @@ public class BattleRoom extends SFSExtension
 			// update DB
 			if( !game.player.isBot() )
 			{
-				trace("battle outcomes:", outcomesList[i].toString());
+				//trace("battle outcomes:", outcomesList[i].toString());
 				// increase daily battles
 				if( game.player.get_battleswins() > 0 && !game.player.inFriendlyBattle && !battleField.map.isOperation )
 				{
