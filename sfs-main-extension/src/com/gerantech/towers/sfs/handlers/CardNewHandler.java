f37d84b6858898d722dfdee9fddc47bdea973458
Mansour <mansurjavadi@gmail.com>
2019-04-20 20:15:55 +0430

    fixed new card bugs

--- sfs-main-extension/src/com/gerantech/towers/sfs/handlers/CardNewHandler.java
+++ sfs-main-extension/src/com/gerantech/towers/sfs/handlers/CardNewHandler.java
@@ -4,18 +4,24 @@ import com.gt.BBGClientRequestHandler;
 import com.gt.Commands;
 import com.gt.towers.Game;
 import com.gt.towers.battle.units.Card;
+import com.gt.towers.constants.MessageTypes;
+import com.gt.towers.utils.maps.IntIntMap;
+import com.gt.utils.DBUtils;
 import com.smartfoxserver.v2.entities.User;
 import com.smartfoxserver.v2.entities.data.ISFSObject;
 
 /**
  * @author ManJav
- *
  */
 public class CardNewHandler extends BBGClientRequestHandler
 {
 	public CardNewHandler() {}
 	public void handleClientRequest(User sender, ISFSObject params)
     {
-		send(Commands.CARD_NEW, Card.addNew((Game)sender.getSession().getProperty("core"), params.getInt("c")), params, sender);
+    	Game game = (Game)sender.getSession().getProperty("core");
+    	int response = Card.addNew(game, params.getInt("c"));
+    	if( response == MessageTypes.RESPONSE_SUCCEED )
+			DBUtils.getInstance().insertResources(game.player, new IntIntMap(params.getInt("c") + ":1"));
+		send(Commands.CARD_NEW, response, params, sender);
     }
 }
